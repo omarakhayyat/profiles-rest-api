6b4393a435409fdb27a4648a2cfbedbc890b40ab
omarakhayyat <omar.a.khayyat@gmail.com>
2021-01-25 23:31:19 +0400

    enabled django admin for the user profile model

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -8,31 +8,32 @@ from django.contrib.auth.models import BaseUserManager
 class UserProfileManager(BaseUserManager):
     """Helps Django work with our custom user model."""
 
-    def create_user(self,email,name,password=None):
-        """Creates a new user profile object."""
+    def create_user(self, email, name, password=None):
+        """Creates a new user profile."""
 
         if not email:
             raise ValueError('Users must have an email address.')
 
-            email = self.normalize_email(email)
-            user = self.model(email=email, name=name)
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name,)
 
-            user.set_password(password)
-            user.save(using=self._db)
+        user.set_password(password)
+        user.save(using=self._db)
 
-            return user
+        return user
 
-            def create_superuser(self,email,name,passowrd):
-                """Creates and save a new superuser with given details."""
+    def create_superuser(self, email, name, password):
+        """Creates and saves a new superuser with given details."""
 
-                user = self.create_user(email, name, password)
+        user = self.create_user(email, name, password)
 
-                user.is_superuser = True
-                user.is_staff = True
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
 
-                user.save(using=self._db)
+        return user
 
-
+        
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Represents a "user profile" inside our system."""
 
